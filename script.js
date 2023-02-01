4b6cea01883e3134cb1e374b6623c83c3048ead1
Matt Osborne <MattJOzzy@gmail.com>
2023-02-01 17:40:17 -0600

    created basic javascript functionality, html layout, and css styling

---
+++ script.js
@@ -0,0 +1,134 @@
+//PSEUDO-CODE
+    //User clicks start button
+    //Timer starts counting down & user is presented with a question
+    //Incorrect answers reduce the timer
+    //Game ends when all questions answered or timer reaches 0
+    //User can enter their initials to save score
+
+var timeEl = document.querySelector("#time");
+var secondsLeft = 10;
+
+function setTime() {
+    var timerInterval = setInterval(function(){
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + " seconds left.";
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            alert("out of time");
+            //create a function called endGame and call it here
+        }
+    }, 1000);
+};
+
+// setTime();
+
+var startButton = document.getElementById('startButton')
+var nextButton = document.getElementById('nextButton')
+var questionContainerEl = document.getElementById('questionContainer')
+var questionEl = document.getElementById('question')
+var answerButtonsEl = document.getElementById('answerButtons')
+
+let randomQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setQuestion()
+})
+
+function startGame(){
+    startButton.classList.add('hide')
+    randomQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
+    questionContainerEl.classList.remove('hide')
+    setQuestion()
+}
+
+function setQuestion() {
+    resetAnswers()
+    showQuestion(randomQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionEl.innerText = question.question
+    question.answers.forEach(answer => {
+        var button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsEl.appendChild(button)
+    })
+}
+
+function resetAnswers() {
+    nextButton.classList.add('hide')
+    while (answerButtonsEl.firstChild) {
+        answerButtonsEl.removeChild(answerButtonsEl.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    var selectedButton = e.target
+    var correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsEl.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (randomQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+    
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('incorrect')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('incorrect')
+}
+
+var questions = [
+
+    {
+        question: 'Question 1',
+        answers: [
+            {text: 'Answer 1', correct: true},
+            {text: 'Answer 2', correct: false}
+        ]
+    },
+    {
+        question: 'Question 2',
+        answers: [
+            {text: 'Answer 1', correct: true},
+            {text: 'Answer 2', correct: false}
+        ]
+    },
+    {
+        question: 'Question 3',
+        answers: [
+            {text: 'Answer 1', correct: true},
+            {text: 'Answer 2', correct: false}
+        ]
+    },
+    {
+        question: 'Question 4',
+        answers: [
+            {text: 'Answer 1', correct: true},
+            {text: 'Answer 2', correct: false}
+        ]
+    },
+]
